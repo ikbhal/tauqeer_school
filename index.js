1c0746b4a8dd32732484bb47c2d108786b61c3d5
root <iqbalforall@gmail.com>
2022-05-26 15:14:57 +0000

    add apps

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3786
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended:true}))
+
 app.get('/', (req, res) => {
-  res.send('Tauqeer School!')
+  //res.send('Tauqeer School!');
+  res.redirect('/index.html');
 })
 
 app.listen(port, () => {
